56a52f525e2b58e8144aa01bf1db257e6c00ddd3
Sergio Delgado <sergio.d.olarte@gmail.com>
2024-04-13 08:40:32 -0500

    Commit N1

---
+++ 03_kata_javascript_avanzado/01_practicas_clase/01_nodejs/01_helloworld.js
@@ -0,0 +1,8 @@
+console.log("Hola Mundo de NodeJS");
+
+
+// Ciclo Infinito / Finito
+for (let i = 0; i < 10; i++) {
+    console.log("Ayuda? " + (i +1));
+    console.log("Random Number: " + math.floor(Number(math.random() * i)));
+}
---
+++ 03_kata_javascript_avanzado/01_practicas_clase/01_nodejs/02_ejemplo_array.js
@@ -0,0 +1,7 @@
+
+
+const palabras = "Bienvenidos,al,módulo,de,NodeJS";
+
+const arregloPalabras = palabras.split(',');
+
+console.log (arregloPalabras);
